7be4fdce14fe2e2f816452e8abf97f5157c11295
TDMwangi <teddymwangi65@gmail.com>
2024-09-19 04:18:43 +0300

    feat: add variable visibility_timeout

--- modules/variables.tf
+++ modules/variables.tf
@@ -2,3 +2,8 @@ variable "name" {
   description = "The name of the queue"
   type        = string
 }
+
+variable "visibility_timeout" {
+  description = "The visibility timeout for the queue. The default is 30 seconds"
+  type        = number
+}
