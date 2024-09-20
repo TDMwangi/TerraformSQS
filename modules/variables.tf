5280ca12c5f58482f866610885c8082466d790b4
TDMwangi <teddymwangi65@gmail.com>
2024-09-20 05:47:03 +0300

    feat: add variable message_retention

--- modules/variables.tf
+++ modules/variables.tf
@@ -7,3 +7,8 @@ variable "visibility_timeout" {
   description = "The visibility timeout for the queue. The default is 30 seconds"
   type        = number
 }
+
+variable "message_retention" {
+  description = "The number of seconds SQS retains a message. The default is 345600 (4 days)"
+  type        = number
+}
