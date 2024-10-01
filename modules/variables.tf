0d547e4469fa918c5ebfcc2defbb662c032775e0
TDMwangi <teddymwangi65@gmail.com>
2024-10-01 05:35:44 +0300

    feat: add variable wait_time

--- modules/variables.tf
+++ modules/variables.tf
@@ -22,3 +22,8 @@ variable "max_message_size" {
   description = "How many bytes a message can contain before SQS rejects it. The default is 262144 (256 KiB)"
   type        = number
 }
+
+variable "wait_time" {
+  description = "The time for which a ReceiveMessage call will wait for a message to arrive. The default is 0 seconds"
+  type        = number
+}
